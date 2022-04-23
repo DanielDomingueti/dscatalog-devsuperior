f7ee217e250cdfcb7a3b98fc6b34b21996723589
DanielDomingueti <danieldomingueti@hotmail.com>
2022-04-23 19:54:55 -0300

    backend created

---
+++ backend/src/test/java/com/domingueti/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingueti.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
