83b5c74a2be8ab45c7c897f669c5d1a135398551
DanielDomingueti <danieldomingueti@hotmail.com>
2022-04-24 10:23:30 -0300

    feat: update category

--- backend/src/main/java/com/domingueti/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/domingueti/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.domingueti.dscatalog.dto.CategoryDTO;
 import com.domingueti.dscatalog.entities.Category;
 import com.domingueti.dscatalog.repositories.CategoryRepository;
-import com.domingueti.dscatalog.services.exceptions.EntityNotFoundException;
+import com.domingueti.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,7 +30,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category model = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category model = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(model);
 	}
 
@@ -38,6 +40,19 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = categoryRepository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category category = categoryRepository.getOne(id);
+			category.setName(dto.getName());
+			category = categoryRepository.save(category);
+			return new CategoryDTO(category);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID not found " + id);
+		}
 	}
 
 }
