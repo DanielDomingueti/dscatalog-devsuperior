b1543aa12c58fbae904e8d15cd1d9fc82e5b01b4
DanielDomingueti <danieldomingueti@hotmail.com>
2022-04-24 08:01:35 -0300

    feat: h2-console

--- backend/src/main/java/com/domingueti/dscatalog/entities/Category.java
+++ backend/src/main/java/com/domingueti/dscatalog/entities/Category.java
@@ -3,6 +3,12 @@ package com.domingueti.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +18,14 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
