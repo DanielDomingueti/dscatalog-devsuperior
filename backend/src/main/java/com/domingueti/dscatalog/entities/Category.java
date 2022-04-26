68e2d2b88a6915a2e46420aff073bd67c507718b
DanielDomingueti <danieldomingueti@hotmail.com>
2022-04-25 21:04:12 -0300

    feat: Auditing data

--- backend/src/main/java/com/domingueti/dscatalog/entities/Category.java
+++ backend/src/main/java/com/domingueti/dscatalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.domingueti.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -28,6 +32,22 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
 	
 	@Override
 	public int hashCode() {
