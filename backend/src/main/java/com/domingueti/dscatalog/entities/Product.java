f0cab668d5e5a9dbc0affc198dd07fb5e9629240
DanielDomingueti <danieldomingueti@hotmail.com>
2022-04-26 05:44:41 -0300

    feat: entity product

---
+++ backend/src/main/java/com/domingueti/dscatalog/entities/Product.java
@@ -0,0 +1,66 @@
+package com.domingueti.dscatalog.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="tb_product")
+public class Product implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Getter Long id;
+	
+	private @Getter @Setter String name;
+	
+	private @Getter @Setter String description;
+	
+	private @Getter @Setter Double price;
+	
+	private @Getter @Setter String imgUrl;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private @Getter @Setter Instant date;
+	
+	//conjunto nao aceita repeticoes
+	private @Getter Set<Category> categories = new HashSet<>();
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
